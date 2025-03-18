48935b0ba0d53218b535e9f344b16e66b20533c0
nicholslavajo <167741603+NicholsG6@users.noreply.github.com>
2025-03-18 08:19:39 +0800

    Add files via upload

---
+++ DTI/signup.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+            <!-- Left Side: Login Form -->
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="login-container">
+                    <div class="login-header">
+                        <h2 class="text-dark" style="font-weight: 700;">DTI <br> E-CERTIFICATION <br> SYSTEM</h2>
+                        <p class="text-black">Welcome to DTI</p>
+                    </div>
+                    
+                    <form name ="signupForm" action="" method="POST" onsubmit="return validateForm()">
+                            <div class="form-group mb-3">
+                                <label for="">Email</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Confirm Password</label>
+                                <input type="password" name="confirm_password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" name="signup_btn" class="btn btn-primary">Sign Up</button>
+                            </div>
+                        </form>  
+                </div>
+            </div>
+
+
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="slider-container">
+                    <div class="image-slider">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+                        <img src="img/cert.png" alt="Certificate">
+
+                    </div>
+                    
+                </div>
+            </div>
+            <img style="z-index: -2; position: absolute; width: 100%; height: 110%; border-radius: 50%; margin-left: -70%;" src="img/bg.jpg" alt="">
+    <div class="container vh-100 d-flex align-items-center">
+        <div class="row w-100">
+
+        </div>
+        
+    </div>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
