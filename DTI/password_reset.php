f3f1279d39af6e77dbff559eeb13bd9cb7ff095a
RG <jamescaballero.dti@gmail.com>
2025-03-19 14:21:40 +0800

    Update password_reset.php

--- DTI/password_reset.php
+++ DTI/password_reset.php
@@ -9,21 +9,27 @@ session_start();
     <title>Forgot Password</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">    
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">    
 </head>
 <body>
+    <img style="z-index: -2; position: absolute; width: 100%; height: 110%;" src="img/DTI-BACKGROUND.png" alt="">
+    
     <div class="container vh-100 d-flex align-items-center justify-content-center">
-        <div class="col-md-4 p-4 shadow rounded text-center ">
+        <div class="col-md-4 p-4 shadow rounded text-center">
             <h2 class="text-white fw-bold">DTI E-CERTIFICATION SYSTEM</h2>
-            <form action="password_reset_code.php" method="POST">
-                            <div class="form-group mb-3">
-                                <input type="text" name="email" class="form-control" placeholder="Enter Email Address">
-                            </div>
-                            <div class="form-group mb-3">
-                                <button type="submit" name="password_reset" class="btn btn-primary">Send Reset Password link</button>
-                            </div>
+            <p class="text-white mb-4">Enter your email to reset your password</p>
+            
+            <form action="password_change.php" method="POST">
+                <div class="form-group mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="Enter Email Address" required>
+                </div>
+                <div class="form-group mb-3">
+                    <button type="submit" name="password_reset" class="btn btn-primary">Send Password Reset Link</button>
+                </div>
+            </form>
+            
+            <a href="index.php" class="password-reset-back-link">Back to Login</a>
         </div>
-        <img style="z-index: -2; position: absolute; width: 100%; height: 110%;" src="img/DTI-BACKGROUND.png" alt="">
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
